9919a30356ccd4aaef17ce09979e4ac869ccd215
Chase Berry <chase@bryx.com>
2015-07-25 21:30:55 -0400

    Added the constructor that takes a JsonTokenizer and creates a JsonArray

--- src/main/kotlin/edu/csh/chase/sprint/json/JsonArray.kt
+++ src/main/kotlin/edu/csh/chase/sprint/json/JsonArray.kt
@@ -1,9 +1,37 @@
 package edu.csh.chase.sprint.json
 
+import java.util.*
+
 class JsonArray() {
 
-    constructor(tokener: JsonTokener) : this() {
+    private val array = ArrayList<Any?>()
 
+    constructor(tokener: JsonTokener) : this() {
+        if (tokener.nextClean() != '[') {
+            throw tokener.syntaxError("A JSONArray text must start with '['")
+        }
+        if (tokener.nextClean() != ']') {
+            tokener.back()
+            while (true) {
+                if (tokener.nextClean() == ',') {
+                    tokener.back()
+                    array.add(null)
+                } else {
+                    tokener.back()
+                    array.add(tokener.nextValue())
+                }
+                when (tokener.nextClean()) {
+                    ',' -> {
+                        if (tokener.nextClean() == ']') {
+                            return
+                        }
+                        tokener.back()
+                    }
+                    ']' -> return
+                    else -> throw tokener.syntaxError("Expected a ',' or ']'")
+                }
+            }
+        }
     }
 
 }
