099323b395e91ddbc4c8f6a2ea025663d188bb55
Chase Berry <chase@bryx.com>
2015-07-26 21:51:34 -0400

    Added another test for the JsonObject

--- src/test/kotlin/json/JsonObjectTest.kt
+++ src/test/kotlin/json/JsonObjectTest.kt
@@ -1,5 +1,6 @@
 package json
 
+import edu.csh.chase.sprint.json.JsonException
 import edu.csh.chase.sprint.json.JsonObject
 import org.junit.Test
 import kotlin.test.assertEquals
@@ -10,6 +11,18 @@ class JsonObjectTest() {
     Test fun emptyObjectTest() {
         val obj = JsonObject()
         assertEquals(0, obj.size)
+        assertEquals("{}", obj.toString())
+        assertEquals("{\n}", obj.toString(true))
+    }
+
+    Test fun invalidJson1Test() {
+        val invalidJson = "{\"key\":\"value}"
+        try {
+            val obj = JsonObject(invalidJson)
+        } catch(unterminatedString: JsonException) {
+            return
+        }
+        assert(false, "Should have thrown a Unterminated string JsonException")
     }
 
 }
