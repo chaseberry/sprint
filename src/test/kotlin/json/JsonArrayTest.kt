22cd95a11aa450f6a3fdbcc18bfb630b0e15d833
Chase Berry <chase@bryx.com>
2015-07-29 21:11:48 -0400

    Added a test to create json from a String

--- src/test/kotlin/json/JsonArrayTest.kt
+++ src/test/kotlin/json/JsonArrayTest.kt
@@ -1,10 +1,12 @@
 package json
 
 import edu.csh.chase.sprint.json.JsonArray
+import edu.csh.chase.sprint.json.JsonException
 import org.junit.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 class JsonArrayTest() {
 
@@ -17,6 +19,23 @@ class JsonArrayTest() {
         assertEquals(1024, array[0, 1024])
         assertFalse(0 in array)
         assertEquals("[]", array.toString())
+    }
+
+    Test fun jsonFromStringTest() {
+        val jsonArrayString = "[0,\"A String\",15.8, true, null]"
+        try {
+            val array = JsonArray(jsonArrayString)
+
+            assertEquals(5, array.size)
+
+            assertEquals(0, array[0])
+            assertEquals("A String", array[1])
+            assertEquals(15.8, array.getDouble(2))
+            assertTrue(array.getBoolean(3, false))
+            assertNull(array[4])
+        } catch(invalidJson: JsonException) {
+            assert(false, "JsonArray failed to create")
+        }
     }
 
 }
