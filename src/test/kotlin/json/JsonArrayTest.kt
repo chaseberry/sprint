f78509f1a71825f2b6c1f3e6a5c5546f56d7218e
Chase Berry <chase@bryx.com>
2015-07-29 21:06:22 -0400

    Added a base JsonArrayTest class with a single test for an empty JsonArray

---
+++ src/test/kotlin/json/JsonArrayTest.kt
@@ -0,0 +1,21 @@
+package json
+
+import edu.csh.chase.sprint.json.JsonArray
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
+
+class JsonArrayTest() {
+
+    fun emptyJsonArrayTest() {
+        val array = JsonArray()
+
+        assertEquals(0, array.size)
+
+        assertNull(array[0])
+        assertEquals(1024, array[0, 1024])
+        assertFalse(0 in array)
+        assertEquals("[]", array.toString())
+    }
+
+}
