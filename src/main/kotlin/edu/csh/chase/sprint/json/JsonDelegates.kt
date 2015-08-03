1a6d49337d41445f164b813688b1869d2287303a
Chase Berry <chase@bryx.com>
2015-08-02 23:08:00 -0400

    Added delegates for notNull variants that take a function that gives the key and returns an any. Considering just making it a default value, not a function

--- src/main/kotlin/edu/csh/chase/sprint/json/JsonDelegates.kt
+++ src/main/kotlin/edu/csh/chase/sprint/json/JsonDelegates.kt
@@ -26,4 +26,23 @@ public object JsonDelegates {
 
     }
 
+    public fun <T>notNullObjectVal(jsonObject: JsonObject, defaultValue: (String) -> Any = { key -> key }): JsonObjectValNotNull<T>
+            = JsonObjectValNotNull(jsonObject, defaultValue)
+
+    private open class JsonObjectValNotNull<T>(protected val jsonObject: JsonObject, val defaultValue: (String) -> Any) :
+            ReadOnlyProperty<T, Any> {
+        override fun get(thisRef: T, desc: PropertyMetadata): Any {
+            return jsonObject[desc.name, defaultValue(desc.name)]
+        }
+
+    }
+
+    private class JsonObjectVarNotNull<T>(jsonObject: JsonObject, defaultValue: (String) -> Any) :
+            JsonObjectValNotNull<T>(jsonObject, defaultValue), ReadWriteProperty<T, Any> {
+        override fun set(thisRef: T, desc: PropertyMetadata, value: Any) {
+            jsonObject[desc.name] = value
+        }
+
+    }
+
 }
