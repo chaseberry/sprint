b29db29ab4cabfee6d56810821602b8f4b7dfb12
chase <chase@bryx.com>
2015-08-04 08:40:42 -0400

    Added an arrayList of Headers to the request object

--- src/main/kotlin/edu/csh/chase/sprint/Request.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Request.kt
@@ -1,9 +1,11 @@
 package edu.csh.chase.sprint
 
 import com.squareup.okhttp.RequestBody
+import edu.csh.chase.sprint.parameters.Header
 import edu.csh.chase.sprint.parameters.UrlBody
-import java.util.*
+import java.util.ArrayList
 
-data class Request(val url: String, val requestType: RequestType, val urlParams: UrlBody?, val body: RequestBody?, var extraData: Any? = null) {
-
+data class Request(val url: String, val requestType: RequestType,
+                   val urlParams: UrlBody? = null, val body: RequestBody? = null,
+                   val headers: ArrayList<Header>? = null, var extraData: Any? = null) {
 }
