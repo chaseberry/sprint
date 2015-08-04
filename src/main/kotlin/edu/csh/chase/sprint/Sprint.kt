c6d7d988b73fc2d671d3a352b8ab6fd8dca13daf
chase <chase@bryx.com>
2015-08-04 09:09:25 -0400

    Added a OkHttp client to be reused for base implementation

--- src/main/kotlin/edu/csh/chase/sprint/Sprint.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Sprint.kt
@@ -1,17 +1,29 @@
 package edu.csh.chase.sprint
 
-import edu.csh.chase.sprint.parameters.Body
+import com.squareup.okhttp.OkHttpClient
 import edu.csh.chase.sprint.parameters.Header
 import edu.csh.chase.sprint.parameters.UrlBody
-import java.util.*
+import java.util.ArrayList
+import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
 
 public object Sprint {
 
-    public fun get(url: String, urlParameters: UrlBody, headers: ArrayList<Header>?, requestFinished: (Request, Response) -> Unit) {
+    private val client: OkHttpClient by Delegates.lazy {
+        val client = OkHttpClient()
+        client.setConnectTimeout(30, TimeUnit.SECONDS)
+        client.setWriteTimeout(30, TimeUnit.SECONDS)
+        client.setReadTimeout(30, TimeUnit.SECONDS)
+        client
+    }
+
+
+    public fun get(url: String, urlParameters: UrlBody, headers: ArrayList<Header>?,
+                   requestFinished: ((Request, Response) -> Unit)?) {
 
     }
 
-    public fun get(request: Request) {
+    public fun get(request: Request): RequestProcessor {
 
     }
 
