33385be1b1db34e3f7db17b40443714f0001bb01
chase <chase@bryx.com>
2015-08-04 09:09:51 -0400

    Work on the execution and callbacks of requests

--- src/main/kotlin/edu/csh/chase/sprint/RequestProcessor.kt
+++ src/main/kotlin/edu/csh/chase/sprint/RequestProcessor.kt
@@ -1,10 +1,18 @@
 package edu.csh.chase.sprint
 
+import com.squareup.okhttp
+import com.squareup.okhttp.Call
+import com.squareup.okhttp.Callback
 import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Response
+import java.io.IOException
 import com.squareup.okhttp.Request as OkRequest
+import com.squareup.okhttp.Response as OkResponse
 
-class RequestProcessor(val request: Request, val client: OkHttpClient, val successListener: SprintSuccess?,
-                       val failureListener: SprintFailure?) {
+class RequestProcessor(val request: Request, private val client: OkHttpClient, val successListener: SprintSuccess?,
+                       val failureListener: SprintFailure?) : Callback {
+
+    var currentCall: Call? = null
 
     private fun buildOkRequest(): OkRequest {
         val builder = OkRequest.Builder()
@@ -16,6 +24,23 @@ class RequestProcessor(val request: Request, val client: OkHttpClient, val succe
         }
         request.headers?.forEach { builder.addHeader(it.name, it.value) }
         return builder.build()
+    }
+
+    fun executeRequest() {
+        val okRequest = buildOkRequest()
+        currentCall = client.newCall(okRequest)
+        currentCall!!.enqueue(this)
+    }
+
+    fun cancelRequest() {
+        currentCall?.cancel()
+        currentCall = null
+    }
+
+    override fun onFailure(request: okhttp.Request?, e: IOException?) {
+    }
+
+    override fun onResponse(response: Response?) {
     }
 
 }
