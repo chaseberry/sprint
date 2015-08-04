4f5f628c17d4dc23ff0e24e541aedcf27172cca1
Chase Berry <chase@bryx.com>
2015-08-03 22:12:28 -0400

    Reorded the parameters for the Request Constructor

--- src/main/kotlin/edu/csh/chase/sprint/Request.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Request.kt
@@ -4,4 +4,6 @@ import com.squareup.okhttp.RequestBody
 import edu.csh.chase.sprint.parameters.UrlBody
 import java.util.*
 
-data class Request(val url: String, urlParams: UrlBody?, val requestType: RequestType, val body: RequestBody?, var extraData: Any? = null)
+data class Request(val url: String, val requestType: RequestType, val urlParams: UrlBody?, val body: RequestBody?, var extraData: Any? = null) {
+
+}
