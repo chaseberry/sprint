56d21df8a4e37086d1b7002eed3c258d8db4969e
Chase Berry <chase@bryx.com>
2015-08-03 22:05:58 -0400

    Added a RequestBody field to the Request object

--- src/main/kotlin/edu/csh/chase/sprint/Request.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Request.kt
@@ -1,7 +1,8 @@
 package edu.csh.chase.sprint
 
+import com.squareup.okhttp.RequestBody
 import java.util.*
 
-data class Request(val url: String, val requestType: RequestType, var extraData: Any? = null) {
+data class Request(val url: String, val requestType: RequestType, val body: RequestBody, var extraData: Any? = null) {
 
 }
