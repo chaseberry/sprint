b0216debcaaa84d4ed57d7d50035327d0444f64d
chase <chase@bryx.com>
2015-08-04 11:35:20 -0400

    Added a val that returns the body as Json

--- src/main/kotlin/edu/csh/chase/sprint/Response.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Response.kt
@@ -2,7 +2,17 @@ package edu.csh.chase.sprint
 
 import com.squareup.okhttp.Headers
 import com.squareup.okhttp.ResponseBody
+import edu.csh.chase.sprint.json.JsonBase
+import edu.csh.chase.sprint.json.parseJsonString
 
-data class Response(val statusCode: Int, val responseData: ResponseBody?, val headers: Headers?) {
+data class Response(val statusCode: Int, val responseBody: ResponseBody?, val headers: Headers?) {
+
+    val bodyAsJson: JsonBase?
+        get() {
+            if (responseBody == null) {
+                return null
+            }
+            return parseJsonString(String(responseBody.bytes()))
+        }
 
 }
