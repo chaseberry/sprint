fd16e7b886c86dcad00b28947857210e39b5ee51
chase <chase@bryx.com>
2015-08-04 11:48:38 -0400

    Added a RequestSerializer interface to check the type of a parameter passed in and serialize the data to a Request Body

---
+++ src/main/kotlin/edu/csh/chase/sprint/RequestSerializer.kt
@@ -0,0 +1,11 @@
+package edu.csh.chase.sprint
+
+import com.squareup.okhttp.RequestBody
+
+interface RequestSerializer {
+
+    fun isValidType(requestData: Any?): Boolean
+
+    fun serialize(requestData: Any?): RequestBody
+
+}
