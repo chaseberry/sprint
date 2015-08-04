9a9fa190ca269467c3bd93f49137e72575e45681
Chase Berry <chase@bryx.com>
2015-08-03 22:11:39 -0400

    Added more parameters to the Request Class

--- src/main/kotlin/edu/csh/chase/sprint/Request.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Request.kt
@@ -1,8 +1,7 @@
 package edu.csh.chase.sprint
 
 import com.squareup.okhttp.RequestBody
+import edu.csh.chase.sprint.parameters.UrlBody
 import java.util.*
 
-data class Request(val url: String, val requestType: RequestType, val body: RequestBody, var extraData: Any? = null) {
-
-}
+data class Request(val url: String, urlParams: UrlBody?, val requestType: RequestType, val body: RequestBody?, var extraData: Any? = null)
