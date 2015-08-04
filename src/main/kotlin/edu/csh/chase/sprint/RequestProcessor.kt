5057cde68319bd706f2bed14bccef8676075760d
Chase Berry <chase@bryx.com>
2015-08-03 22:16:12 -0400

    Started work on the building of the OkHttpRequest

--- src/main/kotlin/edu/csh/chase/sprint/RequestProcessor.kt
+++ src/main/kotlin/edu/csh/chase/sprint/RequestProcessor.kt
@@ -1,12 +1,20 @@
 package edu.csh.chase.sprint
 
 import com.squareup.okhttp.OkHttpClient
+import com.squareup.okhttp.Request as OkRequest
 
-class RequestProcessor(val request: Request, val client: OkHttpClient) {
+class RequestProcessor(val request: Request, val client: OkHttpClient, val successListener: SprintSuccess?,
+                       val failureListener: SprintFailure?) {
 
-    init {
-        val builder = com.squareup.okhttp.Request.Builder()
-        //builder.addHeader()
+    private fun buildOkRequest(): OkRequest {
+        val builder = OkRequest.Builder()
+        //TODO make sure the URL is still valid if the urlParams is null
+        builder.url(request.url + request.urlParams?.toString())
+        when (request.requestType) {
+            RequestType.Get -> builder.get()
+            RequestType.Post -> builder.post(request.body)
+        }
+        
     }
 
 }
