93c6d5527290c4c106998344b9f8f02890f79c3c
chase <chase@bryx.com>
2015-08-04 08:46:48 -0400

    Will now take a List<Any?> or Map<String, Any?> and convert it to a JsonObject and use that for the request body

--- src/main/kotlin/edu/csh/chase/sprint/parameters/JsonBody.kt
+++ src/main/kotlin/edu/csh/chase/sprint/parameters/JsonBody.kt
@@ -2,10 +2,16 @@ package edu.csh.chase.sprint.parameters
 
 import com.squareup.okhttp.MediaType
 import com.squareup.okhttp.RequestBody
+import edu.csh.chase.sprint.json.JsonArray
 import edu.csh.chase.sprint.json.JsonBase
+import edu.csh.chase.sprint.json.JsonObject
 import okio.BufferedSink
 
 class JsonBody(private val jsonValue: JsonBase) : RequestBody() {
+
+    constructor(map: Map<String, Any?>) : this(JsonObject(map))
+
+    constructor(list: List<Any?>) : this(JsonArray(list))
 
     override fun contentType(): MediaType? {
         return MediaType.parse("application/json; charset=utf-8")
