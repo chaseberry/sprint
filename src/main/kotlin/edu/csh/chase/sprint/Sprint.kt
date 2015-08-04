3c9231edd04802d50c7c67223a2da0bb5e313422
Chase Berry <chase@bryx.com>
2015-08-03 22:15:54 -0400

    Added a sample Get function

---
+++ src/main/kotlin/edu/csh/chase/sprint/Sprint.kt
@@ -0,0 +1,18 @@
+package edu.csh.chase.sprint
+
+import edu.csh.chase.sprint.parameters.Body
+import edu.csh.chase.sprint.parameters.Header
+import edu.csh.chase.sprint.parameters.UrlBody
+import java.util.*
+
+public object Sprint {
+
+    public fun get(url: String, urlParameters: UrlBody, headers: ArrayList<Header>?, requestFinished: (Request, Response) -> Unit) {
+
+    }
+
+    public fun get(request: Request) {
+
+    }
+
+}
