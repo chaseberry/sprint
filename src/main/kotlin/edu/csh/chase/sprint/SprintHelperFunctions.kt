743611bf3cc4b85894a6c5e76e0ba0ccaea3aa8b
chase <chase@bryx.com>
2015-08-05 12:22:03 -0400

    Added a function to have a RequestBody right from a JsonBase

--- src/main/kotlin/edu/csh/chase/sprint/SprintHelperFunctions.kt
+++ src/main/kotlin/edu/csh/chase/sprint/SprintHelperFunctions.kt
@@ -1,6 +1,9 @@
 package edu.csh.chase.sprint
 
 import com.squareup.okhttp.Headers
+import com.squareup.okhttp.RequestBody
+import edu.csh.chase.sprint.json.JsonBase
+import edu.csh.chase.sprint.parameters.JsonBody
 
 fun headers(vararg headers: Pair<String, String>): Headers.Builder {
     val builder = Headers.Builder()
@@ -18,3 +21,8 @@ fun buildEndpoint(urlBase: String, endpoint: String): String {
     }
     return builtString
 }
+
+val JsonBase.toRequestBody: RequestBody
+    get() {
+        return JsonBody(this)
+    }
