19561a6d85448a1c91a6a8c893a2568a392c9235
chase <chase@bryx.com>
2015-08-05 12:31:38 -0400

    Added tests for checking Url generation

---
+++ src/test/kotlin/sprint/RequestTest.kt
@@ -0,0 +1,31 @@
+package sprint
+
+import com.squareup.okhttp.OkHttpClient
+import edu.csh.chase.sprint.GetRequest
+import edu.csh.chase.sprint.RequestProcessor
+import edu.csh.chase.sprint.parameters.UrlParameters
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class RequestTest() {
+
+    Test fun buildUrlRequestTest() {
+        val request = GetRequest(
+                url = "https://test.com",
+                urlParams = UrlParameters("key" to "value")
+        )
+        val proccessor = RequestProcessor(request, OkHttpClient(), null)
+        val builtRequest = proccessor.buildOkRequest()
+        assertEquals("https://test.com/?key=value&", builtRequest.urlString())
+    }
+
+    Test fun buildUrlNoParametersTest() {
+        val request = GetRequest(
+                url = "https://test.com"
+        )
+        val proccessor = RequestProcessor(request, OkHttpClient(), null)
+        val builtRequest = proccessor.buildOkRequest()
+        assertEquals("https://test.com/", builtRequest.urlString())
+    }
+
+}
