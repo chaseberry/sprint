1465a3f64e8b9152af6e0f1bf8c1b8d49be48315
chase <chase@bryx.com>
2015-08-05 12:36:40 -0400

    Changed the name from responseBody to body

--- src/main/kotlin/edu/csh/chase/sprint/Response.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Response.kt
@@ -5,17 +5,17 @@ import com.squareup.okhttp.ResponseBody
 import edu.csh.chase.sprint.json.JsonBase
 import edu.csh.chase.sprint.json.parseJsonString
 
-data class Response(val statusCode: Int, val responseBody: ResponseBody?, val headers: Headers?) {
+data class Response(val statusCode: Int, val body: ResponseBody?, val headers: Headers?) {
 
     val bodyAsJson: JsonBase?
         get() {
-            if (responseBody == null) {
+            if (body == null) {
                 return null
             }
-            return parseJsonString(String(responseBody.bytes()))
+            return parseJsonString(String(body.bytes()))
         }
 
-    val bodyAsString: String? = if (responseBody == null) null else String(responseBody.bytes())
+    val bodyAsString: String? = if (body == null) null else String(body.bytes())
 
     val successful: Boolean
         get() {
