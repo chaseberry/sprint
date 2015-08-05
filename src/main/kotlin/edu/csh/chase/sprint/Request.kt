2126b27bd7881f1551f9a5b189c60d141352af66
chase <chase@bryx.com>
2015-08-05 09:58:37 -0400

    Added a helper function to create a Get Request

--- src/main/kotlin/edu/csh/chase/sprint/Request.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Request.kt
@@ -8,3 +8,15 @@ data class Request(val url: String, val requestType: RequestType,
                    val urlParams: UrlParameters? = null, val body: RequestBody? = null,
                    val headers: Headers.Builder? = null, var extraData: Any? = null) {
 }
+
+fun GetRequest(url: String,
+               urlParams: UrlParameters? = null,
+               headers: Headers.Builder? = null, extraData: Any? = null): Request {
+    return Request(
+            url = url,
+            requestType = RequestType.Get,
+            urlParams = urlParams,
+            headers = headers,
+            extraData = extraData
+    )
+}
