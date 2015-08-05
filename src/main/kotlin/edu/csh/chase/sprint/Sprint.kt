6b95d8a6eaa32c02a1d11521b31223b4dfc44433
chase <chase@bryx.com>
2015-08-05 09:48:36 -0400

    Added delete requests

--- src/main/kotlin/edu/csh/chase/sprint/Sprint.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Sprint.kt
@@ -72,4 +72,18 @@ public object Sprint {
                 body = body, extraData = extraData), requestFinished)
     }
 
+    public fun delete(url: String, urlParameters: UrlParameters ? = null, headers: Headers.Builder ? = null,
+                      body: RequestBody? = null, extraData: Any ? = null, requestFinished: ((Request, Response) -> Unit )):
+            RequestProcessor {
+        return executeRequest(Request(url = url, requestType = RequestType.Delete, urlParams = urlParameters, headers = headers,
+                body = body, extraData = extraData), requestFinished)
+    }
+
+    public fun delete(url: String, urlParameters: UrlParameters ? = null, headers: Headers.Builder ? = null,
+                      body: RequestBody? = null, extraData: Any ? = null, requestFinished: SprintListener):
+            RequestProcessor {
+        return executeRequest(Request(url = url, requestType = RequestType.Delete, urlParams = urlParameters, headers = headers,
+                body = body, extraData = extraData), requestFinished)
+    }
+
 }
