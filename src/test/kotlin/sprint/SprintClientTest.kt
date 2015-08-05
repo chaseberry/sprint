a840bab1eebcf85ee0982708734acb1d3b4d6b2f
chase <chase@bryx.com>
2015-08-05 12:18:33 -0400

    Added a skeleton test class for the SprintClient abstraction

---
+++ src/test/kotlin/sprint/SprintClientTest.kt
@@ -0,0 +1,17 @@
+package sprint
+
+import com.squareup.okhttp.OkHttpClient
+import edu.csh.chase.sprint.JsonRequestSerializer
+import edu.csh.chase.sprint.RequestSerializer
+import edu.csh.chase.sprint.SprintClient
+
+class SprintClientTest : SprintClient("Some url here") {
+    
+    override fun configureClient(client: OkHttpClient) {
+
+    }
+
+    override val defaultRequestSerializer: RequestSerializer = JsonRequestSerializer()
+
+
+}
