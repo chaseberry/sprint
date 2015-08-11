38f65a3e23fe97c3ac9c6c4ef0d84521b38b3e8d
chase <chase@bryx.com>
2015-08-11 10:09:21 -0400

    Fixed unit tests. Added a test for checking endpoints built

---
+++ src/test/kotlin/sprint/EndpointTest.kt
@@ -0,0 +1,17 @@
+package sprint
+
+import edu.csh.chase.sprint.buildEndpoint
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+class EndpointTest {
+
+    Test fun testBuildEndpoint() {
+        val base = "https://google.com/"
+        val endpoint = "test"
+        assertEquals("https://google.com/test", buildEndpoint(base, endpoint))
+        assertNotEquals("https://google.comtest", buildEndpoint(base, endpoint))
+    }
+
+}
