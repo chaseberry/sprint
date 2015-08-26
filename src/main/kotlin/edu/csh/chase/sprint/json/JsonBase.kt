d67ef8a2b848b0a119ab466e85177f22b8d5773e
chase <chase@bryx.com>
2015-08-26 09:10:54 -0400

    Move work on the traversal of JsonStructs

--- src/main/kotlin/edu/csh/chase/sprint/json/JsonBase.kt
+++ src/main/kotlin/edu/csh/chase/sprint/json/JsonBase.kt
@@ -4,4 +4,29 @@ abstract class JsonBase : JsonSerializable {
 
     abstract val size: Int
 
+    fun traverse(compoundKey: String, delim: String = ":"): Any? {
+        val key = compoundKey.splitBy(delim).iterator()
+
+    }
+
+    fun traverse(compoundKey: String, default: Any, delim: String = ":"): Any {
+        return traverse(compoundKey = compoundKey, delim = delim) ?: default
+    }
+
+    private fun traverseArray(key: Iterator<String>, array: JsonArray): Any? {
+        if (!key.hasNext()) {
+            return null
+        }
+
+
+    }
+
+    private fun traverseObject(key: Iterator<String>, `object`: JsonObject): Any? {
+        if (!key.hasNext()) {
+            return null
+        }
+        val value = `object`[key.next()]
+        return value
+    }
+
 }
