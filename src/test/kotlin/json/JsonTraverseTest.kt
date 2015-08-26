3febc5264d13d3f9a941a1d956e5fa85d004f17b
chase <chase@bryx.com>
2015-08-26 09:38:02 -0400

    Added a test for traversing array

--- src/test/kotlin/json/JsonTraverseTest.kt
+++ src/test/kotlin/json/JsonTraverseTest.kt
@@ -1,21 +1,35 @@
 package json
 
+import edu.csh.chase.sprint.json.JsonArray
 import edu.csh.chase.sprint.json.JsonObject
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class JsonTraverseTest {
 
-    Test fun traverseObjectTest() {
-        val obj = JsonObject()
+    Test fun traverseBaseObjectTest() {
+        val jsonObject = JsonObject()
+        val obj = jsonObject
         obj["key"] = "value"
-        obj["test"] = "always"
+        obj["test"] = 15
 
         assertEquals("value", obj.traverse("key"))
         assertEquals("value", obj.traverse("key:test"))
-        assertEquals("always", obj.traverse("test"))
+        assertEquals(15, obj.traverse("test"))
         assertEquals(null, obj.traverse("invalid"))
         assertEquals("default", obj.traverse(compoundKey = "invalid", default = "default"))
+    }
+
+    Test fun traverseBaseArrayTest() {
+        val arr = JsonArray("value", 15)
+
+        assertEquals("value", arr.traverse("0"))
+        assertEquals("value", arr.traverse("0:1"))
+        assertEquals(15, arr.traverse("1"))
+        assertEquals(null, arr.traverse("3"))
+        assertEquals(null, arr.traverse("invalid"))
+        assertEquals("default", arr.traverse(compoundKey = "invalid", default = "default"))
+        assertEquals("default", arr.traverse(compoundKey = "-1", default = "default"))
     }
 
 }
