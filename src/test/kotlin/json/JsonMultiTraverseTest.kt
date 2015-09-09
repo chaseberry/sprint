e825c4c96f36d567f3cb6b3e07e8bb21ce435053
chase <chase@bryx.com>
2015-09-09 16:26:12 -0400

    Added more asserts to the test case

--- src/test/kotlin/json/JsonMultiTraverseTest.kt
+++ src/test/kotlin/json/JsonMultiTraverseTest.kt
@@ -7,13 +7,16 @@ import kotlin.test.assertNull
 
 class JsonMultiTraverseTest {
 
-    val obj1 = JsonObject().put("key", "value").put("k2", "v2").put("k3", "k3")
+    val obj1 = JsonObject().put("key", "value").put("k2", "v2").put("k3", "v3")
     val obj2 = JsonObject().put("a", "b").put("c", "d").put("e", "f").put("obj", obj1)
 
     Test fun traverseMultiBasic() {
         assertEquals(obj1.traverseMulti("k", "ke", "key"), "value")
         assertNull(obj1.traverseMulti("k", "ke"))
         assertEquals(obj2.traverseMulti("z", "y", "v:xz", "obj:key"), "value")
+        assertEquals(obj2.traverseMulti("obj:key", "v:xw"), "value")
+        assertEquals(obj2.traverseMulti("obj:k3", "v:wq"), "v3")
+        assertEquals(obj2.traverseMulti("v:wq", "y:t", "obj:k2"), "v2")
     }
 
 }
