b49c8167b275ac3ac38b058c32c0740fe68fb4b6
chase <chase@bryx.com>
2015-09-11 09:57:41 -0400

    Added a TODO The best part is IntelliJ tells me I have a TODO and asks if I REALLY want to commit it.

--- src/main/kotlin/edu/csh/chase/sprint/json/JsonTokener.kt
+++ src/main/kotlin/edu/csh/chase/sprint/json/JsonTokener.kt
@@ -1,31 +1,6 @@
 package edu.csh.chase.sprint.json
 
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStream
-import java.io.InputStreamReader
-import java.io.Reader
-import java.io.StringReader
-
-/*
-Copyright (c) 2002 JSON.org
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-The Software shall be used for Good, not Evil.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+import java.io.*
 
 /**
  * A JsonTokener takes a source string and extracts characters and tokens from
@@ -34,6 +9,7 @@ SOFTWARE.
  * @author JSON.org
  * @version 2014-05-03
  */
+//TODO see if there is a better way of parsing
 class JsonTokener(private var reader: Reader) {
 
     private var character = 1L
