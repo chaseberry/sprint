a35e285a2d38506ec6ff4a0876bf0f5a8f81e6f9
chase <chase@bryx.com>
2015-09-11 10:25:50 -0400

    JavaDocs

--- src/main/kotlin/edu/csh/chase/sprint/json/json.kt
+++ src/main/kotlin/edu/csh/chase/sprint/json/json.kt
@@ -2,11 +2,25 @@ package edu.csh.chase.sprint.json
 
 object json {
 
+    /**
+     * Constructs a JsonArray from a list of elements
+     * If any provided element is not a valid json type it will be skipped
+     *
+     * @param elements A list of Any? elements to put into a JsonArray
+     * @return A JsonArray containing only valid elements from the provided list
+     */
     fun get(vararg elements: Any?): JsonArray {
         return JsonArray(*elements)
     }
 
-    fun invoke(json: () -> Array<Pair<String,Any?>>): JsonObject {
+    /**
+     * Constructs a JsonObject from a Lambda that returns an array of Pair<String, Any?>
+     * Provided pairs with invalid json types will be ignored
+     *
+     * @param json A Function that returns an array of key, value pairs
+     * @return A JsonObject with only valid pairs from the provided lambda
+     */
+    fun invoke(json: () -> Array<Pair<String, Any?>>): JsonObject {
         return JsonObject(*json())
     }
 
