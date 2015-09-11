2dbb49e43b3f31119eb88f35c603626159a25a74
chase <chase@bryx.com>
2015-09-11 10:18:13 -0400

    Javadocs

--- src/main/kotlin/edu/csh/chase/sprint/json/JsonBase.kt
+++ src/main/kotlin/edu/csh/chase/sprint/json/JsonBase.kt
@@ -2,9 +2,22 @@ package edu.csh.chase.sprint.json
 
 abstract class JsonBase : JsonSerializable {
 
+    /**
+     * A value that equals how large this JsonBase is
+     */
     abstract val size: Int
+
     var delim = ":"
 
+    /**
+     * Attempts to walk down a chain of Objects and Arrays based on a compoundKey
+     * Compound keys will be split by the delim(base of ":")
+     * If it encounters a dead route it will return what it last found, including the array or object it's checking
+     * An int will be parses as an array index, all else will be object keys
+     *
+     * @param compoundKey A compound key in the form of key:key2:key3 to dig down into
+     * @return The value that was found by the provided key, or null if nothing was found
+     */
     fun traverse(compoundKey: String): Any? {
         val key = compoundKey.splitBy(delim).iterator()
         return when (this) {
@@ -14,10 +27,26 @@ abstract class JsonBase : JsonSerializable {
         }
     }
 
+    /**
+     * Attempts to walk down a chain of Objects and Arrays based on a compoundKey
+     * Compound keys will be split by the delim(base of ":")
+     * If it encounters a dead route it will return what it last found, including the array or object it's checking
+     * An int will be parses as an array index, all else will be object keys
+     *
+     * @param compoundKey A compound key in the form of key:key2:key3 to dig down into
+     * @param default A default value to return if null was encountered
+     * @return The value that was found by the provided key, or null if nothing was found
+     */
     fun traverse(compoundKey: String, default: Any): Any {
         return traverse(compoundKey = compoundKey) ?: default
     }
 
+    /**
+     * Traverses this JsonBase with multiple keys
+     *
+     * @param keys A list of keys to check
+     * @return The first found value or null if no key matched
+     */
     fun traverseMulti(vararg keys: String): Any? {
         for (key in keys) {
             return traverse(compoundKey = key) ?: continue
@@ -25,6 +54,13 @@ abstract class JsonBase : JsonSerializable {
         return null
     }
 
+    /**
+     * Traverses this JsonBase with a default value if a null was found
+     *
+     * @param default A default if null was found
+     * @param keys A list of keys to check
+     * @return The first found value or default if none worked
+     */
     fun traverseMultiWithDefault(default: Any, vararg keys: String): Any {
         return traverseMulti(keys = *keys) ?: default
     }
