a2b14ef0defa77aaf072fe1bb10efe0b49c1944f
Chase <chase@csh.rit.edu>
2016-07-22 17:59:29 -0400

    Added a class for WebSocketDisconnect Constants

---
+++ src/main/kotlin/edu/csh/chase/sprint/websockets/WebSocketDisconnect.kt
@@ -0,0 +1,10 @@
+package edu.csh.chase.sprint.websockets
+
+object WebSocketDisconnect {
+
+    val normalClosure = 1000
+    val goingAway = 1001
+    val protocolError = 1002
+    val invalidDataType = 1003
+
+}
