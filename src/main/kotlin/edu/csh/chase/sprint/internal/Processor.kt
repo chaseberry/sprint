917992e8cce695c413b900f927624b0906e40f97
Chase <chase@csh.rit.edu>
2016-07-22 19:26:28 -0400

    Added an OkHttp client as a param

--- src/main/kotlin/edu/csh/chase/sprint/internal/Processor.kt
+++ src/main/kotlin/edu/csh/chase/sprint/internal/Processor.kt
@@ -3,10 +3,11 @@ package edu.csh.chase.sprint.internal
 import edu.csh.chase.sprint.Request
 import edu.csh.chase.sprint.RequestType
 import okhttp3.MediaType
+import okhttp3.OkHttpClient
 import okhttp3.Request as OkRequest
 import okhttp3.RequestBody
 
-abstract class Processor(val request: Request) {
+abstract class Processor(val request: Request, protected val client: OkHttpClient) {
 
     fun buildOkRequest(): OkRequest {
         val builder = OkRequest.Builder()
