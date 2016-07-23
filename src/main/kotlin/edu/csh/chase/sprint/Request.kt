ff488d04c31091305e3703a3de40212cd8c43520
Chase <chase@csh.rit.edu>
2016-07-23 15:08:21 -0400

    Added a lazy val to create an OkHttpRequest

--- src/main/kotlin/edu/csh/chase/sprint/Request.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Request.kt
@@ -2,11 +2,34 @@ package edu.csh.chase.sprint
 
 import edu.csh.chase.sprint.parameters.UrlParameters
 import okhttp3.Headers
+import okhttp3.MediaType
 import okhttp3.RequestBody
+import okhttp3.Request as OkRequest
 
 data class Request(val url: String, val requestType: RequestType,
                    var urlParams: UrlParameters? = null, var body: RequestBody? = null,
-                   val headers: Headers.Builder = Headers.Builder(), var extraData: Any? = null)
+                   val headers: Headers.Builder = Headers.Builder(), var extraData: Any? = null) {
+
+    val okHttpRequest: OkRequest by lazy {
+        val builder = OkRequest.Builder()
+        //TODO make sure the URL is still valid if the urlParams is null
+        builder.url(if (urlParams != null) {
+            url + urlParams.toString()
+        } else {
+            url
+        })
+        when (requestType) {
+            RequestType.Get -> builder.get()
+            RequestType.Post -> builder.post(body ?: RequestBody.create(MediaType.parse("text/plain"), ""))
+            RequestType.Put -> builder.put(body ?: RequestBody.create(MediaType.parse("text/plain"), ""))
+            RequestType.Delete -> builder.delete(body)
+        }
+        builder.headers(headers.build())
+
+        builder.build()
+    }
+
+}
 
 fun GetRequest(url: String,
                urlParams: UrlParameters? = null,
