a363c4d48f576ba79361a3f70fe4e4a8fdd41142
Chase <chase@csh.rit.edu>
2016-07-24 22:51:01 -0400

    Made the websocket object self sufficient. Still a `prototype` API

--- src/main/kotlin/edu/csh/chase/sprint/websockets/WebSocket.kt
+++ src/main/kotlin/edu/csh/chase/sprint/websockets/WebSocket.kt
@@ -2,25 +2,36 @@ package edu.csh.chase.sprint.websockets
 
 import edu.csh.chase.sprint.Request
 import edu.csh.chase.sprint.Response
+import okhttp3.OkHttpClient
 import okhttp3.RequestBody
 import okhttp3.Response as OkResponse
 import okhttp3.ResponseBody
+import okhttp3.ws.WebSocketCall
 import okhttp3.ws.WebSocketListener
 import okio.Buffer
 import java.io.IOException
 import java.util.*
 import okhttp3.ws.WebSocket as OkWebSocket
 
-class WebSocket(protected val request: Request, callback: WebSocketCallbacks?, val retryCount: Int, val retryOnServerClose: Boolean) : WebSocketListener {
+open class WebSocket(protected val request: Request,
+                     callback: WebSocketCallbacks?,
+                     private val client: OkHttpClient,
+                     val retryCount: Int = 4,
+                     val retryOnServerClose: Boolean = false,
+                     val autoConnect: Boolean = true) : WebSocketListener {
 
     private val listeners: ArrayList<WebSocketCallbacks> = ArrayList<WebSocketCallbacks>()
     private var socket: OkWebSocket? = null
     private var currentRetry: Int = retryCount
-    var closed: Boolean = false
-        private set
+    private var call: WebSocketCall? = null
+    val closed: Boolean
+        get() = socket == null
 
     init {
         callback?.let { listeners.add(it) }
+        if (autoConnect) {
+            connect()
+        }
     }
 
     fun sendText(text: String) {
@@ -43,9 +54,24 @@ class WebSocket(protected val request: Request, callback: WebSocketCallbacks?, v
         }
     }
 
-    fun close(code: Int, reason: String?) {
-        socket?.close(code, reason)
-        closed = true
+    fun connect() {
+        if (!closed) {
+            //Already connected
+            return
+        }
+
+        call = WebSocketCall.create(client, request.okHttpRequest)
+        call!!.enqueue(this)
+
+    }
+
+    fun disconnect(code: Int, reason: String?) {
+        if (closed) {
+            //Already closed
+            return
+        }
+        socket!!.close(code, reason)
+        socket = null
     }
 
     override fun onOpen(webSocket: OkWebSocket, response: OkResponse) {
@@ -62,26 +88,31 @@ class WebSocket(protected val request: Request, callback: WebSocketCallbacks?, v
 
     override fun onClose(code: Int, reason: String?) {
         listeners.forEach { it.onDisconnect(code, reason) }
+        socket = null
+        if (retryOnServerClose) {
+            doRetry()
+        }
     }
 
     override fun onFailure(exception: IOException, response: OkResponse?) {
-        listeners.forEach { it.onError(exception, response?.let { Response(it) }) }
+        val res = response?.let { Response(it) }
+        listeners.forEach { it.onError(exception, res) }
+        socket = null
+
         doRetry()
     }
 
     override fun onMessage(message: ResponseBody?) {
-        listeners.forEach { it.onMessage(Response(200, message?.bytes(), null)) }
+        val response = Response(200, message?.bytes(), null)
+        listeners.forEach { it.onMessage(response) }
     }
 
     private fun doRetry() {
-        if (closed) {
-            return
-        }
         if (retryCount == noRetry || currentRetry == 0) {
             return
         }
 
-
+        connect()
     }
 
     companion object {
