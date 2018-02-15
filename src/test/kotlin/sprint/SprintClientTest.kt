d1279911584eb1e12bab67f9662785cdd3e44850
Chase <chase@csh.rit.edu>
2018-02-15 11:38:34 -0500

    Added two tests to the SprintClientTest using https://reqres.in

--- src/test/kotlin/sprint/SprintClientTest.kt
+++ src/test/kotlin/sprint/SprintClientTest.kt
@@ -2,15 +2,55 @@ package sprint
 
 import edu.csh.chase.sprint.JsonRequestSerializer
 import edu.csh.chase.sprint.RequestSerializer
+import edu.csh.chase.sprint.Response
 import edu.csh.chase.sprint.SprintClient
 import okhttp3.OkHttpClient
+import org.junit.Test
 
-class SprintClientTest : SprintClient("Some url here") {
+class SprintClientTest : SprintClient("https://reqres.in/api/") {
 
     override fun configureClient(client: OkHttpClient.Builder) {
 
     }
 
     override val defaultRequestSerializer: RequestSerializer = JsonRequestSerializer()
-    
+
+    @Test fun getUserOne() {
+        get(
+            "users/1"
+        ) {
+            assert(it is Response.Success) { "Request should be a Success" }
+
+            when (it) {
+                is Response.Success -> {
+                    assert(200 == it.statusCode) { "StatusCode was not 200" }
+
+                    it.bodyAsJson ?: error("Body was not Json")
+                }
+                is Response.Failure -> {
+
+                }
+            }
+
+        }
+    }
+
+    @Test fun fourOFour() {
+        get(
+            "unknown/23"
+        ) {
+            assert(it is Response.Success) { "Request should be a Success" }
+
+            when (it) {
+                is Response.Success -> {
+                    assert(404 == it.statusCode) { "StatusCode was not 200" }
+
+                }
+                is Response.Failure -> {
+
+                }
+            }
+        }
+    }
+
 }
