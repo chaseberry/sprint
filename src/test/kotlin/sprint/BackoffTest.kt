81d5f120a1f1b791a89e9b12508730fe98bbb063
Chase <chase@csh.rit.edu>
2018-02-22 13:42:44 -0500

    Cleaned up the tests

--- src/test/kotlin/sprint/BackoffTest.kt
+++ src/test/kotlin/sprint/BackoffTest.kt
@@ -10,8 +10,8 @@ class BackoffTest {
         val backoff = BackoffTimeout.Exponential(500, 2, 300000L, 6)
 
         //should output the values 500, 1000, 2000, 4000, 8000, 16000
-        listOf(500L, 1000L, 2000L, 4000L, 8000L, 16000L).forEachIndexed { index, i ->
-            assertEquals(i, backoff.getNextDelay(index))
+        listOf(500L, 1000L, 2000L, 4000L, 8000L, 16000L).forEach {
+            assertEquals(it, backoff.getNextDelay())
         }
 
     }
@@ -19,8 +19,8 @@ class BackoffTest {
     @Test fun testLinearBackoff() {
         val backoff = BackoffTimeout.Linear(0, 100, 300000L, 6)
 
-        listOf(0L, 100L, 200L, 300L, 400L, 500L).forEachIndexed { index, i ->
-            assertEquals(i, backoff.getNextDelay(index))
+        listOf(0L, 100L, 200L, 300L, 400L, 500L).forEach {
+            assertEquals(it, backoff.getNextDelay())
         }
     }
 
