bdd36bb02b67088ca360cf5471fb01e96a235785
Chase <chase@csh.rit.edu>
2018-02-22 20:21:37 -0500

    Removed the ServerResponse abstract class and duplicated the logic for Success, Failure Added a code Int? to get the statusCode or null if it's a connectionError

--- src/main/kotlin/edu/csh/chase/sprint/Response.kt
+++ src/main/kotlin/edu/csh/chase/sprint/Response.kt
@@ -10,23 +10,7 @@ sealed class Response(val request: Request) {
 
     abstract override fun toString(): String
 
-    class Success(request: Request, statusCode: Int, body: ByteArray?, headers: Headers?) : ServerResponse(request, statusCode, body, headers) {
-
-        constructor(request: Request, response: OkResponse) : this(request, response.code(), response.body()?.bytes(), response.headers())
-    }
-
-    class Failure(request: Request, statusCode: Int, body: ByteArray?, headers: Headers?) : ServerResponse(request, statusCode, body, headers) {
-
-        constructor(request: Request, response: OkResponse) : this(request, response.code(), response.body()?.bytes(), response.headers())
-    }
-
-    class ConnectionError(request: Request, val error: IOException) : Response(request) {
-
-
-        override fun toString(): String = error.message ?: "null"
-    }
-
-    abstract class ServerResponse(request: Request, val statusCode: Int, val body: ByteArray?, val headers: Headers?) : Response(request) {
+    class Success(request: Request, val statusCode: Int, val body: ByteArray?, val headers: Headers?) : Response(request) {
 
         constructor(request: Request, response: OkResponse) : this(request, response.code(), response.body()?.bytes(), response.headers())
 
@@ -39,5 +23,32 @@ sealed class Response(val request: Request) {
         override fun toString(): String = bodyAsString ?: "null"
 
     }
+
+    class Failure(request: Request, val statusCode: Int, val body: ByteArray?, val headers: Headers?) : Response(request) {
+
+        constructor(request: Request, response: OkResponse) : this(request, response.code(), response.body()?.bytes(), response.headers())
+
+        val bodyAsJson: JsonBase?
+            get() = bodyAsString?.let { Json.parse(it) }
+
+        val bodyAsString: String?
+            get() = body?.let { String(it) }
+
+        override fun toString(): String = bodyAsString ?: "null"
+
+    }
+
+    class ConnectionError(request: Request, val error: IOException) : Response(request) {
+
+        override fun toString(): String = error.message ?: "null"
+    }
+
+    val code: Int?
+        get() = when (this) {
+            is Success -> statusCode
+            is Failure -> statusCode
+            else -> null
+        }
+
 
 }
