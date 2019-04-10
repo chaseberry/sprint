b23006ec090478709f28e4e767f5bd0e60bf9a2d
Chase <chase@csh.rit.edu>
2019-04-10 09:35:48 -0400

    Removed todo

--- src/main/kotlin/edu/csh/chase/sprint/parameters/UrlParameters.kt
+++ src/main/kotlin/edu/csh/chase/sprint/parameters/UrlParameters.kt
@@ -5,7 +5,6 @@ import java.net.URLEncoder
 class UrlParameters(vararg val pairs: Pair<String, Any>) {
 
     override fun toString(): String {
-        //TODO add an escape function for characters that require escaping
         return "?" + pairs.map {
             "${URLEncoder.encode(it.first, "UTF-8")}=${URLEncoder.encode(it.second.toString(), "UTF-8")}"
         }.joinToString("&")
